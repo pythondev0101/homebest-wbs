c8f98ce4d23623bb8d60106e88ef8292de06c82d
Robert Montemayor <36463571+pythondev0101@users.noreply.github.com>
2020-06-14 19:44:01 +0800

    Update run.py
    Cant work if i set env locally

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from app import create_app,db
 # import subprocess
 
-config_name = os.getenv('FLASK_ENV')
+config_name = 'production'
 app = create_app(config_name)
 
 if __name__ == '__main__':
